4283ca6d133e205a48dd6396a1c8e9b847e7cc41
TheoThenault <theo.thenault@gmail.com>
2023-01-22 13:00:04 +0100

    Création route 'list', verif numéro de page valide

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -4,13 +4,27 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/blog', name: 'blog')]
 class BlogController extends AbstractController
 {
-    #[Route('/blog', name: 'app_blog')]
-    public function index(): Response
+
+    #[Route(
+                        '/list/{nPage}',
+        name:           '_list',
+        requirements:   ['nPage' => '\d+'],
+        defaults:       ['nPage' => 1])
+    ]
+    public function index($nPage): Response
     {
+        if($nPage <= 0)
+        {
+            //TODO? Page 404 personnalisé ?
+            throw new NotFoundHttpException('La page n\'existe pas');
+        }
+
         return $this->render('blog/index.html.twig', [
             'controller_name' => 'BlogController',
         ]);
